da0a14e9078c3b77e781392e27d7b29a94dcb708
madiyarrakhman <ino.coder@gmail.com>
2019-06-06 18:00:16 +0600

    First Commit

---
+++ src/Feed.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Feed extends Component {
+    constructor(props) {
+        super(props);
+    };
+
+    render() {
+        return ('FEED');
+    };
+}
+
+export default Feed;
